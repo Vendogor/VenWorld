49d426299d63513df393803c1c74e99e863bcbc0
Roman Makarov <MR.Vendogor@gmail.com>
2021-07-06 05:34:08 +0300

    Added project

---
+++ VenWorld.cpp
@@ -0,0 +1,12 @@
+﻿// VenWorld.cpp : Defines the entry point for the application.
+//
+
+#include "VenWorld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
